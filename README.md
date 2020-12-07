90d96b9346a57d0c934bbceee3087b96856e48fe
Son Nguyen <ngocsonuit94@gmail.com>
2020-12-07 18:58:18 +0700

    [master] update

--- README.md
+++ README.md
@@ -14,7 +14,8 @@ First clone this repository.
 $ git clone https://github.com/nguyenngocson113/weather-app.git
 ```
 
-Create file .env and add
+Create file .env and add:
+
 REACT_APP_API_KEY=9de243494c0b295cca9337e1e96b00e2
 REACT_APP_WEATHER_API=https://api.openweathermap.org/data/2.5
 
--- src/env.js
+++ src/env.js
@@ -1,4 +1,4 @@
-export const WEATHER_API_KEY = process.env.REACT_APP_API_KEY;
+export const WEATHER_API_KEY = process.env.REACT_APP_API_KEY || '9de243494c0b295cca9337e1e96b00e2';
 export const WEATHER_DOMAIN =
   process.env.REACT_APP_WEATHER_URL || 'https://api.openweathermap.org/data/2.5';
 
